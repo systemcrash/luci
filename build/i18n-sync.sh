6ddfb57e6d27f4c24adf05ab2cb2be3f61eb7994
Paul Donald <newtwen+github@gmail.com>
2024-12-22 16:05:00 +0000

    build: default - add line number hints to .po(t) files
    Defaults to "--ln=full" in sync scripts now (so in future, if you need
    the line numbers for e.g. *.po i18n tools, supply "--ln=full" to the
    scripts to generate the *.po(t) files with line numbers included).
    
    Examples:
    
     # omit line numbers and build *.po(t) for luci-app-example
     build/i18n-sync.sh --ln=never applications/luci-app-example
    
     # include line numbers and build *.po(t) for luci-app-example
     build/i18n-sync.sh --ln=full applications/luci-app-example
    
     # include line numbers and build *.po(t) for luci-app-example and base.pot
     build/i18n-sync.sh -b --ln=full applications/luci-app-example
    
    To change the default, modify the line:
    
     line_numbers="--ln=xxxx"
    
    where "--ln=xxxx" is one of "--ln=never" "--ln=file" or "--ln=full"
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- build/i18n-sync.sh
+++ build/i18n-sync.sh
@@ -2,7 +2,7 @@
 
 print_help() {	
 	echo "Execute as ./build/i18n-sync.sh [-b]" >&2
-	echo "Or run as: ./build/i18n-sync.sh [-b] [module folder e.g. applications/luci-app-example]" >&2
+	echo "Or run as: ./build/i18n-sync.sh [-b] [--ln=<never|file|full>] [module folder e.g. applications/luci-app-example]" >&2
 	echo "Options:"
 	echo "	-b: Generate the base .pot file ( invokes ./build/mkbasepot.sh )"
 }
@@ -12,29 +12,49 @@ print_help() {
 	exit 1
 }
 
-case $1 in
-	-h | --help )
-		print_help
-		exit 0
-		;;
-	-b )
-		./build/mkbasepot.sh
-		shift
-		;;
-esac
+# If you need to set a new default for [--ln=<never|file|full>], set it here:
+line_numbers="--ln=full"
+# line_numbers="--ln=never"
+
+for arg in "$@"; do
+	case $arg in
+		-h | --help )
+			print_help
+			exit 0
+			;;
+		--ln=* )
+			line_numbers="$arg"
+			shift
+			;;
+		--ln )
+			print_help
+			exit 0
+			;;
+		-b )
+			makebase=true
+			shift
+			;;
+	esac
+done
 
 [ -n "$1" ] && set -- "${1%/}"
 
-[ -n "$1" ] || ./build/mkbasepot.sh
+# If we received no parameters, update base.pot (also)
+[ -z "$1" ] && makebase=true
+
+[ "$makebase" = true ] && {
+	[ -n "$line_numbers" ] && ./build/mkbasepot.sh "$line_numbers"
+	[ -z "$line_numbers" ] && ./build/mkbasepot.sh
+}
 
 # Absent a [folder] parameter, use the current path
 find "${1:-.}" -name '*.pot' -and -not -name base.pot | sort | \
 	xargs -P 10 -I{} sh -c '
 		dir="${1%/po/templates/*}"
-		echo "Updating ${1#./} ... "
-		./build/i18n-scan.pl "$dir" > "$1"
+		echo "Updating ${1#./} ... ${2:+with: $2}"
+		./build/i18n-scan.pl "$2" "$dir" > "$1"
 		echo "done"
-	' sh {}
+	' sh {} "${line_numbers}"
 
 	# while read path; do
 	# 	dir="${path%/po/templates/*}"
@@ -51,19 +71,22 @@ if [ -n "$1" ]; then
 		find "$1" -path '*/templates/*.pot' -print0 | xargs -0r stat -f '%N' | \
 			xargs -r -n 1 dirname | \
 			xargs -r -n 1 dirname | sort | \
-			xargs -r -n 1 -P 40 ./build/i18n-update.pl
+			# Note: do not quote ${parameters}
+			xargs -r -n 1 -P 40 ./build/i18n-update.pl ${line_numbers}
 	elif [ "$(uname)" = "Linux" ]; then
 		# Linux-specific commands
 		find "$1" -path '*/templates/*.pot' -printf '%h ' | \
 			xargs -r -n 1 dirname | \
-			xargs -r -n 1 -P 40 ./build/i18n-update.pl
+			# Note: do not quote ${parameters}
+			xargs -r -n 1 -P 40 ./build/i18n-update.pl ${line_numbers}
 	# elif [ "$(uname)" = "SunOS" ]; then
 	# 	# Solaris-specific commands
 	else
 		# GNU specific commands can go here:
 		find "$1" -path '*/templates/*.pot' -printf '%h ' | \
 			xargs -r -n 1 dirname | \
-			xargs -r -n 1 -P 40 ./build/i18n-update.pl
+			# Note: do not quote ${parameters}
+			xargs -r -n 1 -P 40 ./build/i18n-update.pl ${line_numbers}
 	fi
 else
 	# this performs operations on all .po files
